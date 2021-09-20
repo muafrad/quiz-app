05a9417da44f92becb0d8208b34eb16f9b2d85c4
muafrad <muh.afrad9@gmail.com>
2021-09-20 14:11:43 +0600

    Sign Up Conponents Created

---
+++ src/components/SignUp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from './Button';
+import Checkbox from './Checkbox';
+import Form from './Form';
+import Illustration from './Illustration';
+import classes from './styles/SignUp.module.css';
+import TextInput from './TextInput';
+
+const SignUp = () => {
+    return (
+        <>
+            <h1>Create an account</h1>
+            <div className="column">
+                <Illustration />
+                <Form className={`${classes.signup}`}>
+                    <TextInput type="text" placeholder="Enter name" icon="person" />
+                    <TextInput type="text" placeholder="Enter email" icon="alternate_email" />
+                    <TextInput type="password" placeholder="Enter password" icon="lock" />
+                    <TextInput type="password" placeholder="Enter password" icon="lock_clock" />
+                    <Checkbox text='I agree to the Terms &amp; Conditions' />
+                    <Button>Submit Now</Button>
+                    <div class="info">
+                        Already have an account? <a href="login.html">Login</a> instead.
+                    </div>
+                </Form>
+            </div>
+        </>
+    );
+};
+
+export default SignUp;
